7b2c707a69a7d14eb9f518bae1103608ee6480ca
Nadine241999 <107752958+Nadine241999@users.noreply.github.com>
2022-06-19 11:03:47 +0200

    Update Mars_Rover_P2.java
    Signed-off-by: Nadine241999 <107752958+Nadine241999@users.noreply.github.com>

--- Mars_Rover_P2.java
+++ Mars_Rover_P2.java
@@ -1,215 +1,152 @@
 /**************************
 //simple code
-//start date : 18/6/2022 12:00 PM
-//end date: 18/6/2022 12:51 PM
+//Functional Programming
 
 ***************************/
 import java.io.*;
 import java.util.*;
-public class Main
-{
+   class rover{
+       int x_axis = 0;
+    int y_axis = 0;
+    String directions = "north";
+    String Command = "F";
+     List<List<Integer>> obstacles; 
+     boolean stopped= false;
 
-  public static void main (String[]args)
-  {
-  Integer[] obsacles_x = new Integer[]{1, 3, 0, 8};
-  Integer[] obsacles_y = new Integer[]{7, 3, 6, 2};
-  List<Integer> X_obstacles = new ArrayList<>(Arrays.asList(obsacles_x));
-  List<Integer> Y_obstacles = new ArrayList<>(Arrays.asList(obsacles_y));
+   public  boolean isValid(int x, int y){
+    for(List<Integer> obstacle : obstacles ){
+        if(obstacle.get(0)==x && obstacle.get(1)==y){
+            stopped=true;
+            return false;
+        }
+    }
+    return true;
+   }
+   
+   public void move(int movement)
+   {
   
-  int x_axis = 0;
-  int y_axis = 0;
-  String directions = "North";
-  String Command = "F";
-  Scanner sc = new Scanner(System.in);
-  System.out.print("Enter X axis: ");
- x_axis = sc.nextInt();
- System.out.print("Enter  y axis: "); 
- y_axis = sc.nextInt();
- //System.out.print("Enter direction: ");
- //directions = sc.next();
-  System.out.print("Enter Command: ");
- Command = sc.next();
- for(int i = 0 ; i<Command.length();i++)
- {
- if(Command.charAt(i) == 'F' || Command.charAt(i) == 'f'){
-  
-      
- if(directions == "North" || directions == "north" || directions == "NORTH" || directions == "NoRth" || directions =="NOrth")
- {
-  if(X_obstacles.contains(x_axis) && Y_obstacles.contains(y_axis+1) ){
-      System.out.print("Obstacle !. Stopped at : ");
-      break;
-  }
-  directions = "North";
-  y_axis = y_axis+1;
- }
-else if(directions == "South" || directions == "south" || directions == "SOUTH"|| directions == "SoUth" || directions == "SOuth")
-{
-  if(X_obstacles.contains(x_axis) && Y_obstacles.contains(y_axis-1) ){
-      System.out.print("Obstacle !. Stopped at : ");
-      break;
-  }
-directions = "South";
-y_axis = y_axis-1;
-}
- else if(directions == "East" || directions == "EAST" || directions == "EAst"|| directions == "EaSt" || directions == "east" || directions == "EaSt")
-{
-  if(X_obstacles.contains(x_axis+1) && Y_obstacles.contains(y_axis) ){
-      System.out.print("Obstacle !. Stopped at : ");
-      break;
-  }
-directions = "East";
-x_axis = x_axis+1;
-}
-else if(directions == "West" || directions == "WEST" || directions == "WEst" || directions == "WeST" || directions == "west" || directions == "weST")
-{
- if(X_obstacles.contains(x_axis-1) && Y_obstacles.contains(y_axis) ){
-      System.out.print("Obstacle !. Stopped at : ");
-      break;
-  }   
-directions = "West";
-x_axis = x_axis-1;
+    if (directions.equals("north")  && isValid(x_axis,y_axis + movement)) 
+    {
+        y_axis +=  movement;
+    } 
+    else if (directions.equals("south") && isValid(x_axis,y_axis - movement)) 
+    {
+        y_axis -=  movement;
+    } 
+    else if ( directions.equals("east") && isValid(x_axis + movement,y_axis)) 
+    {
+        x_axis +=  movement;
+    } 
+    else if ( directions.equals("west") && isValid(x_axis - movement,y_axis)) 
+    {
+        x_axis -=  movement;
+    }
+
+   }
+   public void rotateLeft()
+   {
+    if (directions.equals("north")) 
+    {
+        directions = "west";
+    } 
+    else if (directions.equals("south")) 
+    {
+        directions = "east";
+    } 
+    else if ( directions.equals("east")) 
+    {
+        directions = "north";
+    } 
+    else if ( directions.equals("west")) 
+    {
+        directions = "south";
+    }
+    }
+    public  void rotateRight()
+   {
+    if (directions.equals("north")) 
+    {
+        directions = "east";
+    } 
+    else if (directions.equals("west")) 
+    {
+        directions = "north";
+    } 
+    else if ( directions.equals("east")) 
+    {
+        directions = "south";
+    } 
+    else if ( directions.equals("south")) 
+    {
+        directions = "west";
+    }
+    }
+   
 }
 
-
- }
- 
- /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
- else if(Command.charAt(i) == 'B' || Command.charAt(i) == 'b')
- {
-
-
-  if(directions == "North" || directions == "north" || directions == "NORTH" || directions == "NoRth" || directions =="NOrth")
- {
-   if(X_obstacles.contains(x_axis) && Y_obstacles.contains(y_axis-1) ){
-      System.out.print("Obstacle !. Stopped at : ");
-      break;
-  }
- directions = "South";
-  y_axis = y_axis-1;
- }
- else if(directions == "South" || directions == "south" || directions == "SOUTH"|| directions == "SoUth" || directions == "SOuth")
-{
-  if(X_obstacles.contains(x_axis) && Y_obstacles.contains(y_axis+1) ){
-      System.out.print("Obstacle !. Stopped at : ");
-      break;
-  }
-directions = "North";
- y_axis = y_axis+1;
-}
- else if(directions == "East" || directions == "EAST" || directions == "EAst"|| directions == "EaSt" || directions == "east" || directions == "EaSt")
-{
-  if(X_obstacles.contains(x_axis-1) && Y_obstacles.contains(y_axis) ){
-      System.out.print("Obstacle !. Stopped at : ");
-      break;
-  }
-directions = "West";
-x_axis = x_axis-1;
-}
-else if(directions == "West" || directions == "WEST" || directions == "WEst" || directions == "WeST" || directions == "west" || directions == "weST")
-{
-  if(X_obstacles.contains(x_axis+1) && Y_obstacles.contains(y_axis) ){
-      System.out.print("Obstacle !. Stopped at : ");
-      break;
-  }
-directions = "East";
-x_axis = x_axis+1;
-}
- 
- 
- 
- }
- else if(Command.charAt(i) == 'R' || Command.charAt(i) == 'r')
- {
+public class Main {
 
  
+  public static void main(String[] args) {
+      rover obj= new rover();
+      obj.obstacles = new ArrayList<>(); 
 
- if(directions == "North" || directions == "north" || directions == "NORTH" || directions == "NoRth" || directions =="NOrth")
- {
-   if(X_obstacles.contains(x_axis+1) && Y_obstacles.contains(y_axis) ){
-      System.out.print("Obstacle !. Stopped at : ");
-      break;
-  }
- directions = "East";
-  x_axis = x_axis+1;
- }
- else if(directions == "South" || directions == "south" || directions == "SOUTH"|| directions == "SoUth" || directions == "SOuth")
-{
-  if(X_obstacles.contains(x_axis-1) && Y_obstacles.contains(y_axis) ){
-      System.out.print("Obstacle !. Stopped at : ");
-      break;
-  }
-directions = "West";
-x_axis = x_axis-1;
-}
- else if(directions == "East" || directions == "EAST" || directions == "EAst"|| directions == "EaSt" || directions == "east" || directions == "EaSt")
-{
-  if(X_obstacles.contains(x_axis) && Y_obstacles.contains(y_axis-1) ){
-      System.out.print("Obstacle !. Stopped at : ");
-      break;
-  }
-directions = "South";
-y_axis = y_axis-1;
-}
-else if(directions == "West" || directions == "WEST" || directions == "WEst" || directions == "WeST" || directions == "west" || directions == "weST")
-{
-if(X_obstacles.contains(x_axis) && Y_obstacles.contains(y_axis+1) ){
-      System.out.print("Obstacle !. Stopped at : ");
-      break;
-  }
-directions = "North";
-y_axis = y_axis+1;
-}
- 
- 
- 
- 
- }//hna
- else if (Command.charAt(i) == 'L'|| Command.charAt(i) == 'l')
- {
+      Scanner sc = new Scanner(System.in);
+    System.out.print("Enter X axis: ");
+               obj.x_axis = sc.nextInt();
+    System.out.print("Enter  y axis: ");
+                        obj.y_axis = sc.nextInt();
+      System.out.print("Enter direction: ");
+    obj.directions = sc.next();
+    System.out.print("Enter Command: ");
+    obj.Command = sc.next();
 
+    Integer x_obstacle;
+    Integer y_obstacle;
+    System.out.print("Enter obstacles: ");
+    x_obstacle=sc.nextInt();
+    y_obstacle=sc.nextInt();
+    while(x_obstacle!= -1 || y_obstacle != -1 )
+    {
+        List<Integer> obstacle = new ArrayList<>();
+        obstacle.add(x_obstacle);
+        obstacle.add(y_obstacle);
+        obj.obstacles.add(obstacle);
+        x_obstacle=sc.nextInt();
+        y_obstacle=sc.nextInt();
+    }
+
+    obj.directions = obj.directions.toLowerCase();
+    obj.Command = obj.Command.toLowerCase();
  
- if(directions == "North" || directions == "north" || directions == "NORTH" || directions == "NoRth" || directions =="NOrth")
- {
-  if(X_obstacles.contains(x_axis-1) && Y_obstacles.contains(y_axis) ){
-      System.out.print("Obstacle !. Stopped at : ");
-      break;
+   for(int i=0 ;i<obj.Command.length();i++)
+   {
+       
+       char ch=obj.Command.charAt(i);
+
+       switch (ch) {
+           case 'f':
+               obj.move(1);
+               break;
+               case 'b':
+               obj.move(-1);
+               break;
+               case 'l':
+               obj.rotateLeft();
+               break;
+               case 'r':
+               obj.rotateRight();
+               break;
+           default:
+               break;
+       }
+       if(obj.stopped){
+        System.out.print("Obstacle !. Stopped at : ");
+           break;
+       }
+   }
+
+    System.out.print("Ans: " + "(" + obj.x_axis + ", " + obj.y_axis + ")" + obj.directions);
   }
-  directions = "West";
-  x_axis = x_axis-1;
- 
- }
-else if (directions == "South" || directions == "south" || directions == "SOUTH"|| directions == "SoUth" || directions =="SOuth")
-{
-  if(X_obstacles.contains(x_axis+1) && Y_obstacles.contains(y_axis) ){
-      System.out.print("Obstacle !. Stopped at : ");
-      break;
-  }
-directions = "East";
-x_axis = x_axis+1;
-}
-else if(directions == "East" || directions == "EAST" || directions == "EAst"|| directions == "EaSt" || directions == "east" || directions == "EaSt")
-{
-  if(X_obstacles.contains(x_axis) && Y_obstacles.contains(y_axis+1) ){
-      System.out.print("Obstacle !. Stopped at : ");
-      break;
-  }
-directions = "North";
-y_axis = y_axis +1;
-}
-else if(directions == "West" || directions == "WEST"|| directions == "WEst" || directions == "WeST" || directions == "west" || directions == "weST")
-{
-  if(X_obstacles.contains(x_axis) && Y_obstacles.contains(y_axis-1) ){
-      System.out.print("Obstacle !. Stopped at : ");
-      break;
-  }
-directions = "South";
-y_axis = y_axis -1;
-}
- }
- 
- }
- System.out.print("(" + x_axis +", "+ y_axis +")" +  directions);
- 
-}
 }
